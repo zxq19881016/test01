8929540d3236d66eaccfbcefbd0fe9fae0248c8f
zhaoxiaoqiang <zhaoxiaoqiang@netinfosz.com>
2019-01-22 15:45:19 +0800

    创建项目代码

---
+++ src/main/java/com/netinfo/GreetingController.java
@@ -0,0 +1,14 @@
+package com.netinfo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return  "ssafas";
+	}
+}
